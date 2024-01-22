350c810b18d1cdac8b9c4aee7e6ce1ef8bbe63a2
Ahd-Kabeer-Hadi <ahd.kabeerpi@gmail.com>
2024-01-23 00:29:25 +0530

    added Menu and initial UI elements

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="relative mt-16 min-h-[326px] pb-32 md:pb-12">
+      <div className="h-[1px] w-full bg-black/10 dark:bg-white/10">
+        <div className="container relative z-10 mt-20 flex w-full max-w-[1024px] flex-col items-start justify-between gap-12 md:h-[236px] md:flex-row">
+          I'm footer
+        </div>
+      </div>
+    </footer>
+  );
+}
