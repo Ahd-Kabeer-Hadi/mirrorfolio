a12d0173cd04c07310ba23aa498657e022aaf387
Ahd-Kabeer-Hadi <ahd.kabeerpi@gmail.com>
2024-07-21 12:43:18 +0530

    added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 
 const syne = Syne({
   weight: "700",
@@ -21,8 +22,10 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Mirrorfolio : A creative reflection of Kabeer Hadi,a Software Engineer from Calicut, India.",
-  description: "Hey, I'm Ahd. Kabeer Hadi, a self-taught software engineer from Calicut, Kerala. India. and this is my eploration of what I can do with technology.",
+  title:
+    "Mirrorfolio : A creative reflection of Kabeer Hadi,a Software Engineer from Calicut, India.",
+  description:
+    "Hey, I'm Ahd. Kabeer Hadi, a self-taught software engineer from Calicut, Kerala. India. and this is my eploration of what I can do with technology.",
 };
 
 export default function RootLayout({
@@ -43,6 +46,28 @@ export default function RootLayout({
         <Toaster />
         <Footer />
         <SpeedInsights />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "napesn24bd");
+    `}
+        </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3XQGN1GJCZ"
+        ></Script>
+        <Script strategy="afterInteractive">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3XQGN1GJCZ');
+`}
+        </Script>
       </body>
     </html>
   );
